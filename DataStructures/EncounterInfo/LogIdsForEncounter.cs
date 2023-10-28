9788d46663b5bfe9837a0569de9fe952b7d9f583
David Duba <david.duba@biofiredx.com>
2023-10-27 18:50:17 -0600

    updated TFB combat Ids

--- DataStructures/EncounterInfo/LogIdsForEncounter.cs
+++ DataStructures/EncounterInfo/LogIdsForEncounter.cs
@@ -166,10 +166,10 @@ public static class LogIdFactory
                         {
                             "Operator IX", new Dictionary<string, List<long>>()
                             {
-                                { "Story 8", new List<long>() { 2939690365747200L, 2942606648541184L } },
-                                { "Story 16", new List<long>() { 2994919350206464L, 2994850630729728L } },
-                                { "Veteran 8", new List<long>() { 2994915055239168L, 2994837745827840L } },
-                                { "Veteran 16", new List<long>() { 2994923645173760L, 2994859220664320L } },
+                                { "Story 8", new List<long>() { 2942606648541184L } },
+                                { "Story 16", new List<long>() { 2994850630729728L } },
+                                { "Veteran 8", new List<long>() { 2994837745827840L } },
+                                { "Veteran 16", new List<long>() { 2994859220664320L } },
                             }
                         },
                         {
@@ -185,21 +185,22 @@ public static class LogIdFactory
                         {
                             "The Terror From Beyond", new Dictionary<string, List<long>>()
                             {
-                                { "Story 8", new List<long>() { 2938891501830144L, 2938895796797440L, 2938887206862848L, 2978340776443904L, 3009943145807872L } },
-                                { "Story 16", new List<long>() { 3025263294152704L, 3025276179054592L, 3025224639447040L, 3025237524348928L, 2994872105566208L } },
-                                { "Veteran 8", new List<long>() { 3025271884087296L, 3025258999185408L, 3025220344479744L, 3025233229381632L, 3014921012903936L } },
-                                { "Veteran 16", new List<long>() { 3025280474021888L, 3025267589120000L, 3025228934414336L, 3025241819316224L, 2994876400533504L } },
+                                { "Story 8", new List<long>() { 2938891501830144L, 2938895796797440L, 2938887206862848L, 2978340776443904, 2988545618739200 } },
+                                { "Story 16", new List<long>() { 3025263294152704L, 3025276179054592L, 3025224639447040L, 3025237524348928L, 3025289063956480 } },
+                                { "Veteran 8", new List<long>() { 3025271884087296L, 3025258999185408L, 3025220344479744L, 3025233229381632L, 3025284768989184L } },
+                                { "Veteran 16", new List<long>() { 3025280474021888L, 3025267589120000L, 3025228934414336L, 3025241819316224L, 3025293358923776L } },
                             }
                         }
                     };
                     encounter.RequiredIdsForKill = new Dictionary<string, Dictionary<string, List<long>>>()
                     {
-                        {                            "The Terror From Beyond", new Dictionary<string, List<long>>()
+                        { 
+                            "The Terror From Beyond", new Dictionary<string, List<long>>()
                             {
                                 { "Story 8", new List<long>() { 3009943145807872L } },
-                                { "Story 16", new List<long>() { 2994872105566208L } },
+                                { "Story 16", new List<long>() { 3014925307871232L } },
                                 { "Veteran 8", new List<long>() { 3014921012903936L } },
-                                { "Veteran 16", new List<long>() { 2994876400533504L } },
+                                { "Veteran 16", new List<long>() { 3014929602838528L } },
                             } }
                     };
                     break;
